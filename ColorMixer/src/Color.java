b9965cecfce0776a7d3cda427fd6151495fa31f8
Ben Holland <benjholla@gmail.com>
2013-02-12 02:56:08 -0600

    comments and refactoring

--- ColorMixer/src/Color.java
+++ ColorMixer/src/Color.java
@@ -5,22 +5,39 @@ public class Color {
 	private double S; // Kubelka-Munk scattering coefficient
 	private double R; // R = Reflectance = 1 + (K/S)-[(K/S)^2+2(K/S)]^0.5 (assuming color is opaque)
 	
+	/**
+	 * Returns the Color's Kubelka-Munk absorption coefficient
+	 * @return
+	 */
 	public double getK(){
 		return K;
 	}
 	
+	/**
+	 * Return's the Color's Kubelka-Munk scattering coefficient
+	 * @return
+	 */
 	public double getS(){
 		return S;
 	}
 	
+	/**
+	 * Returns the Color's Reflectance
+	 * @return
+	 */
 	public double getReflectance(){
 		return R;
 	}
 	
+	/**
+	 * Creates a new Color
+	 * @param K Kubelka-Munk absorption coefficient
+	 * @param S Kubelka-Munk scattering coefficient
+	 */
 	public Color(double K, double S){
 		this.K = K;
 		this.S = S;
-		calculateReflectance(K, S);
+		this.R = calculateReflectance(K, S); 
 	}
 	
 	/**
@@ -31,23 +48,26 @@ public class Color {
 	 * @param colors
 	 */
 	public void mix(Color... colors){
-		
+		// caculate a concentration weight for a equal concentration of all colors in mix
 		double concentration = 1 / (1 + colors.length);
+		
+		// calculate first iteration
 		double K = this.K * concentration;
 		double S = this.S * concentration;
 		
+		// sum the weighted average
 		for(int i=0; i<colors.length; i++){
 			K += colors[i].getK() * concentration;
 			S += colors[i].getS() * concentration;
 		}
 		
+		// update with results
 		this.K = K;
 		this.S = S;
-		
-		calculateReflectance(K, S);
+		this.R = calculateReflectance(K, S);
 	}
 	
-	private void calculateReflectance(double K, double S){
-		this.R = 1.0 + (K/S) - Math.pow(Math.pow((K/S), 2) + 2*(K/S), 0.5);
+	private double calculateReflectance(double K, double S){
+		return 1.0 + (K/S) - Math.pow(Math.pow((K/S), 2.0) + 2.0*(K/S), 0.5);
 	}
 }
